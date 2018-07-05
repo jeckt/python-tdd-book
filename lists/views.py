7f014f305967eab8879a2b3de6d62c5d80b81a74
jeckt <steven@sleepyproductions.com>
2018-07-05 21:39:20 +0000

    Add new list template and routing

--- lists/views.py
+++ lists/views.py
@@ -4,11 +4,12 @@ from lists.models import Item
 
 # TODO(steve): Support more than one list!
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
