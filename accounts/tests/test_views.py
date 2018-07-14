30c0cc9064b59c15eaf17a1e783266cce2903bc7
jeckt <steven@sleepyproductions.com>
2018-07-14 12:27:00 +0000

    Add view tests to send emails for log in

---
+++ accounts/tests/test_views.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from unittest.mock import patch
+import accounts.views
+
+class SendLoginEmailViewTest(TestCase):
+
+    def test_redirects_to_home_page(self):
+        response = self.client.post('/accounts/send_login_email',
+                                    data={'email': 'edith@example.com'})
+        self.assertRedirects(response, '/')
+
+    @patch('accounts.views.send_mail')
+    def test_sends_mail_to_address_from_post(self, mock_send_mail):
+        self.client.post('/accounts/send_login_email',
+                         data={'email': 'edith@example.com'
+        })
+
+        self.assertEqual(mock_send_mail.called, True)
+        (subject, body, from_email, to_list), kwards = mock_send_mail.call_args
+        self.assertEqual(subject, 'Your login link for Superlists')
+        self.assertEqual(from_email, 'noreply@superlists')
+        self.assertEqual(to_list, ['edith@example.com'])
+
+    def test_adds_success_message(self):
+        response = self.client.post('/accounts/send_login_email',
+                            data={'email': 'edith@example.com',
+        }, follow=True)
+
+        message = list(response.context['messages'])[0]
+        self.assertEqual(
+            message.message,
+            "Check your email, we've sent you a link you can use to log in."
+        )
+        self.assertEqual(message.tags, "success")
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.contrib import messages
 
-# Create your views here.
+SUBJECT = 'Your login link for Superlists'
+EMAIL = 'noreply@superlists'
+
+def send_login_email(request):
+    email = request.POST['email']
+    send_mail(SUBJECT,
+              'body text tbc',
+              EMAIL,
+              [email]
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
