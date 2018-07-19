74db608d503d2b92edd387fb29b419b64b22b64d
jeckt <steven@sleepyproductions.com>
2018-07-19 23:40:12 +0000

    add screenshot on failure to FT runner

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,8 +5,13 @@ from selenium.webdriver.common.keys import Keys
 
 import time
 import os
+from datetime import datetime
 
 MAX_WAIT = 10
+
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -19,7 +24,41 @@ class FunctionalTest(StaticLiveServerTestCase):
             reset_database(self.staging_server)
 
     def tearDown(self):
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
+        super().tearDown()
+
+    def _test_has_failed(self):
+        # slightly obscure but couldn't find a better way!
+        return any(error for (method, error) in self._outcome.errors)
+
+    def take_screenshot(self):
+        filename = self._get_filename() + '.png'
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + '.html'
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':','.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+        folder=SCREEN_DUMP_LOCATION,
+        classname=self.__class__.__name__,
+        method=self._testMethodName,
+        windowid=self._windowid,
+        timestamp=timestamp
+    )
 
     def wait(fn):
         def modified_fn(*args, **kwargs):
