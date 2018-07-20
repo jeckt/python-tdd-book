628f94dac9db61c5a6cd6a6e83bf985a9455f91a
jeckt <steven@sleepyproductions.com>
2018-07-20 22:05:19 +0000

    Refactor simple list creation FT to use List Page

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -2,12 +2,15 @@ from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+from .list_page import ListPage
+
 class NewVisitorText(FunctionalTest):
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
+        list_page = ListPage(self)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -15,7 +18,7 @@ class NewVisitorText(FunctionalTest):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.get_item_input_box()
+        inputbox = list_page.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -28,27 +31,28 @@ class NewVisitorText(FunctionalTest):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        list_page.wait_for_row_in_list_table('Buy peacock feathers', 1)
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        inputbox = self.get_item_input_box()
+        inputbox = list_page.get_item_input_box()
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        list_page.wait_for_row_in_list_table('Buy peacock feathers', 1)
+        list_page.wait_for_row_in_list_table('Use peacock feathers to make a fly', 2)
 
         # Satisfied, she goes back to sleep
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
-        inputbox = self.get_item_input_box()
+        list_page = ListPage(self)
+        inputbox = list_page.get_item_input_box()
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        list_page.wait_for_row_in_list_table('Buy peacock feathers', 1)
 
         # She notices that her list has a unique URL
         edith_list_url = self.browser.current_url
@@ -70,10 +74,10 @@ class NewVisitorText(FunctionalTest):
 
         # Francis starts a new list by entering a new item. He 
         # is less interesting than Edith...
-        inputbox = self.get_item_input_box()
+        inputbox = list_page.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy milk')
+        list_page.wait_for_row_in_list_table('Buy milk', 1)
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
