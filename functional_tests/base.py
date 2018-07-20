15198e5f244c6122c021001339f86a635d269f2c
jeckt <steven@sleepyproductions.com>
2018-07-20 21:54:55 +0000

    Create Page objects for list pages, use in sharing FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -15,6 +15,18 @@ SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
 )
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -62,28 +74,9 @@ class FunctionalTest(StaticLiveServerTestCase):
         timestamp=timestamp
     )
 
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
-
     @wait
     def wait_for(self, fn):
         return fn()
-
-    @wait
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
 
     @wait
     def wait_to_be_logged_in(self, email):
@@ -96,16 +89,6 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
-
-    def get_item_input_box(self):
-        return self.browser.find_element_by_id('id_text')
-
-    def add_list_item(self, item_text):
-        num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
-        self.get_item_input_box().send_keys(item_text)
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        item_number = num_rows + 1
-        self.wait_for_row_in_list_table('%d: %s' % (item_number, item_text))
 
     def create_pre_authenticated_session(self, email):
         if self.staging_server:
